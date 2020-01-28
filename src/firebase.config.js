60d194dce67ee9bfceed4d90acbb781b4bbe4398
akter <akter.hossain@nhst.com>
2020-01-28 19:33:34 +0600

    remove llll

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,14 +1,5 @@
 import firebase from 'firebase';
-var firebaseConfig = {
-  apiKey: 'AIzaSyBDTIo1CJf3jlwsgh_VHvgABoBPNZmIAMU',
-  authDomain: 'posterbase-test.firebaseapp.com',
-  databaseURL: 'https://posterbase-test.firebaseio.com',
-  projectId: 'posterbase-test',
-  storageBucket: 'posterbase-test.appspot.com',
-  messagingSenderId: '825441833417',
-  appId: '1:825441833417:web:e199de76b332b1143d9818',
-  measurementId: 'G-00FTEF5P28',
-};
+var firebaseConfig = {};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
