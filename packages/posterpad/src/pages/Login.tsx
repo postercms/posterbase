fdb9ece3339451ab53feeeb7ba9bef8dd1312c6c
akter <akter.hossain@nhst.com>
2020-02-03 10:02:41 +0600

    github token save

--- packages/posterpad/src/pages/Login.tsx
+++ packages/posterpad/src/pages/Login.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export function Login(props) {
+  const [token, setToken] = useState('');
+
+  const onTokenChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setToken(e.target.value);
+  };
+
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    localStorage.authToken = token;
+    setToken('');
+  };
+
   return (
     <div className="flex w-full items-center justify-center h-screen">
       <div className="w-full max-w-xs">
-        <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form
+          onSubmit={onSubmit}
+          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -16,6 +30,13 @@ export function Login(props) {
               id="token"
               type="text"
               placeholder="Github token"
+              value={token}
+              onChange={onTokenChange}
+            />
+            <input
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              value="Login"
             />
           </div>
         </form>
